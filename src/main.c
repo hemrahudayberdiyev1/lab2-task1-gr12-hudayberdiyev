c892d2a469e74931f7fcabf149706e64986669c2
hemrahudayberdiyev1 <hemra7351@gmail.com>
2025-03-25 10:18:09 +0000

    Update

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
 
 void main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	printf ("Hello World!\n");
+	printf ("Intro to Repl.it!\n");
+	printf("CI start!\n");
 }
